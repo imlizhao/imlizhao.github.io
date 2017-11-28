26af30d3299c5bd2c7a651fa070c565e7559ea2d
lijianfei <leejeffery@foxmail.com>
2017-11-28 10:48:25 +0800

    init

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const APP_ID = 'bBsyGlhRwjVbe5VSbHv9W7xP-gzGzoHsz';
+const APP_KEY = 'MsAB8Mb9SJ29zNEkBnIAJJum';
+window.AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+//标签
+const tagStr = '首页';
